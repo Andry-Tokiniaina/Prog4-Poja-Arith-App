cc4c715f4db71590f37d5722e5ec5965566400f9
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-05-27 15:27:26 +0300

    fix: format

--- src/main/java/com/example/demo/service/ArithController.java
+++ src/main/java/com/example/demo/service/ArithController.java
@@ -7,25 +7,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class ArithController {
-    private ArithService service;
+  private ArithService service;
 
-    @GetMapping("/add")
-    public double add(int a, int b){
-        return service.add(a, b);
-    }
+  @GetMapping("/add")
+  public double add(int a, int b) {
+    return service.add(a, b);
+  }
 
-    @GetMapping("/substrate")
-    public  double subtract(int a, int b){
-        return  service.subtract(a, b);
-    }
+  @GetMapping("/substrate")
+  public double subtract(int a, int b) {
+    return service.subtract(a, b);
+  }
 
-    @GetMapping("/multiply")
-    public  double multiply(int a, int b){
-        return  service.multiply(a, b);
-    }
+  @GetMapping("/multiply")
+  public double multiply(int a, int b) {
+    return service.multiply(a, b);
+  }
 
-    @GetMapping("/divide")
-    public  double divide(int a, int b){
-        return  service.divide(a, b);
-    }
+  @GetMapping("/divide")
+  public double divide(int a, int b) {
+    return service.divide(a, b);
+  }
 }
